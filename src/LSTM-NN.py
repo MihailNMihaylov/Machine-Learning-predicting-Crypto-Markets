776dea090775995d16564aeebfa83fa179161fb1
MihailNMihaylov <mihaylovm88@gmail.com>
2021-11-01 21:49:42 +0000

    Imported DataSet and visualized it in plot graph

---
+++ src/LSTM-NN.py
@@ -0,0 +1,18 @@
+#Import necessary packages and libraries
+import pandas as pd
+from matplotlib import pyplot as plt
+
+#Read data set from .csv file
+df = pd.read_csv('BTCPriceDataSet.csv')
+
+price = df[['Close']]
+
+#Visualize data using plot X axis is Date and Y axis is Price in GBP
+plt.figure(figsize = (15,9))
+plt.plot(price)
+plt.xticks(range(0, df.shape[0], 50), df['Date'].loc[::50], rotation=45)
+plt.title('BTC price')
+plt.xlabel('Date')
+plt.ylabel('Price')
+
+plt.show()
