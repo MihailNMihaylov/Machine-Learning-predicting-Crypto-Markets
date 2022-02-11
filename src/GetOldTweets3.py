ba610080783964a4522182490457a5cdf6889b6a
MihailNMihaylov <mihaylovm88@gmail.com>
2022-02-12 01:05:00 +0200

    Imported GetOldTweets3 library to retrieve older tweets (from year 2014 oldest)

---
+++ src/GetOldTweets3.py
@@ -0,0 +1,65 @@
+import datetime
+import subprocess
+import  sys
+
+subprocess.check_call([sys.executable, "-m", "pip", "install", "snscrape"])
+
+import snscrape.modules.twitter as sntwitter
+import csv
+
+
+#Set number for maximum tweets
+maxTweets = 300
+
+#Open (or create if doesn't exist) "GetOldTweets3_result.csv" file to save retrieved tweets by Id, Date and Text
+csvFile = open('GetOldTweets3_result.csv', 'a', newline='', encoding='utf8')
+
+csvWriter = csv.writer(csvFile)
+csvWriter.writerow(['id', 'date', 'tweet'])
+
+#Get tweets from User since a specific date
+#write username as the first parameter without "@"
+#Write date in format "yyyy-mm-dd"
+def Get_tweets_by_user(username, sinceDate):
+
+    #Variable that counts how many tweets are there in a day
+    sameDayTweets = 0
+
+    #Initial date converted from DateTime to use only "yyyy-mm-dd" (without the hour)
+    firstDate = datetime.datetime.now()
+    firstDate = datetime.datetime(firstDate.year, firstDate.month, firstDate.day)
+
+    #Import search query into Twitter search bar and start retrieving and saving tweets into csv file
+    for i,tweet in enumerate(sntwitter.TwitterSearchScraper('from:@{} + since:{}'.format(username, sinceDate)).get_items()):
+
+        #Convert current tweet date to be only "yyyy-mm-dd" (without the hour)
+        tweetDate = datetime.datetime(tweet.date.year, tweet.date.month, tweet.date.day)
+
+        if firstDate != tweetDate:
+            sameDayTweets = 0
+
+        #Check if there are more than 3 tweets in a day from that user
+        if firstDate == tweetDate:
+            sameDayTweets = sameDayTweets + 1
+
+            #Max tweets per day - 3
+            if sameDayTweets > 2:
+                continue
+
+        #if max number of tweets is reached break the loop and stop retrieving
+        if i > maxTweets:
+            break
+
+        csvWriter.writerow([tweet.id, tweetDate, tweet.content])
+        firstDate = tweetDate
+    csvFile.close()
+
+
+#Call the function for all users that have influence on the price or have relevant information about the price
+Get_tweets_by_user("coindesk", "2014-01-01")
+#Get_tweets_by_user("coindeskmarkets", "2018-01-01")
+#Get_tweets_by_user("BTCTN", "2015-08-01")
+#Get_tweets_by_user("btc_archive", "2018-04-01")
+#Get_tweets_by_user("bitcoin", "2014-01-01")
+#Get_tweets_by_user("wublockchain", "2014-01-01")
+#Get_tweets_by_user("blockworks_", "2018-05-01")
