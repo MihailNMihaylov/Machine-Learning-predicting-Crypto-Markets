b1242e0c702107a0452360a212246a5a1fd5b9e2
MihailNMihaylov <mihaylovm88@gmail.com>
2022-02-24 15:18:56 +0200

    Implementing Vader sentiment analizer

--- src/TextBlobSentimentAnalysis.py
+++ src/TextBlobSentimentAnalysis.py
@@ -5,7 +5,7 @@ import sys
 #Installing textblob library if missing
 subprocess.check_call([sys.executable, "-m", "pip", "install", "textblob"])
 
-#Importing Vader and TextBlob
+#Importing TextBlob
 from textblob import TextBlob
 
 
@@ -17,5 +17,5 @@ def sentimentResultTextBlob(sentence):
     print(sentimentScore)
 
 
-#Sample tweets to test the accuracy of the sentiment analysis of Vader and TextBlob
+#Sample tweets to test the accuracy of the sentiment analysis of TextBlob
 sentimentResultTextBlob("Employees are asking to be #PaidInBitcoin, and we listened. With our new Bitcoin Savings Plan, companies can now offer an innovative compensation perk for employees to securely buy, sell, and hold #Bitcoin without incurring any transaction or storage fees. https://bit.ly/3IRnv8g")
---
+++ src/VaderSentimentAnalysis.py
@@ -0,0 +1,24 @@
+#Importing packages and libraries
+import subprocess
+import sys
+
+#Installing textblob library if missing
+subprocess.check_call([sys.executable, "-m", "pip", "install", "vaderSentiment"])
+
+#Importing Vader and TextBlob
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+
+
+#Function for sentiment analysis using Vader
+def sentimentResultVader(sentenceToAnalyze):
+
+    vaderObj = SentimentIntensityAnalyzer()
+
+    sentimentScore = vaderObj.polarity_scores(sentenceToAnalyze)
+
+    print(sentimentScore)
+
+
+
+#Sample tweets to test the accuracy of the sentiment analysis of Vader
+sentimentResultVader("Employees are asking to be #PaidInBitcoin, and we listened. With our new Bitcoin Savings Plan, companies can now offer an innovative compensation perk for employees to securely buy, sell, and hold #Bitcoin without incurring any transaction or storage fees. https://bit.ly/3IRnv8g")
