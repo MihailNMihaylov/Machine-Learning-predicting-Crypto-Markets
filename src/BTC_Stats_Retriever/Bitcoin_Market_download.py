8ce0d9914db60cd963fafc6d94ad532e946d8c70
MihailNMihaylov <mihaylovm88@gmail.com>
2022-03-21 01:56:40 +0200

    Implemented code for automatic download of Bitcoin market stats

---
+++ src/BTC_Stats_Retriever/Bitcoin_Market_download.py
@@ -0,0 +1,13 @@
+#Import and download necessary libraries
+import subprocess
+import  sys
+subprocess.check_call([sys.executable, "-m", "pip", "install", "yfinance"])
+
+import yfinance as yf
+
+#Download Bitcoin market statistics into Dataframe
+bitcoinDF = yf.Ticker("BTC-USD").history(period = "1y", interval = "1d")
+
+#Convert Bitcoin DF into DownloadBTC-USD.csv file located in Datasets Directory
+bitcoinDF.to_csv('../../src/DataSets/DownloadBTC-USD.csv')
+
