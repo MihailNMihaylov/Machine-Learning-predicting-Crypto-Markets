f4ea07d8590ee15df540811cd9b1ce9799ddce4f
MihailNMihaylov <mihaylovm88@gmail.com>
2022-03-30 14:17:36 +0300

    Added model evaluation with RMSE

--- src/Neural_Network/LSTM_NN.py
+++ src/Neural_Network/LSTM_NN.py
@@ -6,11 +6,13 @@ from src.Data_preprocessing import preprocessDataset, MinMaxScaler, pd
 #subprocess.check_call([sys.executable, "-m", "pip", "install", "matplotlib"])
 #subprocess.check_call([sys.executable, "-m", "pip", "install", "seaborn"])
 import matplotlib.pyplot as plt
-
+import math
+from sklearn.metrics import mean_squared_error, mean_absolute_error, explained_variance_score, r2_score
 
 from tensorflow.python.keras.models import Sequential
 from tensorflow.python.keras.layers import Dense, Dropout, LSTM
 from tensorflow.python.keras.callbacks import ModelCheckpoint, EarlyStopping
+from tensorflow.python.keras.models import load_model
 
 dataset = pd.read_csv('../../src/DataSets/CombinedResults.csv')
 price = dataset[['Close']]
@@ -38,9 +40,9 @@ test_Df = processedDf[len(processedDf) - predictionDays:].values.reshape(-1,1)
 
 
 #Rescale Dataset
-train_Scaler = MinMaxScaler(feature_range=(0,10))
+train_Scaler = MinMaxScaler(feature_range=(0, 1))
 train_Scaled = train_Scaler.fit_transform(train_Df)
-test_Scaler = MinMaxScaler(feature_range=(0,10))
+test_Scaler = MinMaxScaler(feature_range=(0, 1))
 test_Scaled = test_Scaler.fit_transform(test_Df)
 
 #ref https://github.com/rohan-paul/MachineLearning-D)eepLearning-Code-for-my-YouTube-Channel/blob/master/Finance_Stock_Crypto_Trading/Bitcoin_Price_Prediction_with_LSTM.ipynb
@@ -82,15 +84,17 @@ checkpoint = ModelCheckpoint(filepath=savedModel, monitor='loss', verbose=1, sav
 #callbacks = [checkpoint, earlystopping]
 
 #Fit/Compile the model
-history = model.fit(trainX, trainY, batch_size=32, epochs=100, verbose=1, shuffle=False, validation_data=(testX, testY), callbacks= checkpoint)
+history = model.fit(trainX, trainY, batch_size=32, epochs=200, verbose=1, shuffle=False, validation_data=(testX, testY), callbacks= checkpoint)
 
 #Plot loss of training and testing dataset
 plt.figure(figsize=(16,7))
+plt.title("Loss of train and test dataset")
 plt.plot(history.history['loss'], label= 'train')
 plt.plot(history.history['val_loss'], label= 'test')
 plt.legend()
 plt.show()
 
+#Make a prediction on the test dataset
 predictedData = model.predict(testX)
 predictedData = test_Scaler.inverse_transform(predictedData.reshape(-1, 1))
 
@@ -98,7 +102,31 @@ actualTestData = test_Scaler.inverse_transform(testY.reshape((-1, 1)))
 
 
 plt.figure(figsize=(16,7))
+plt.title('Predicted vs Actual Test data')
 plt.plot(predictedData, 'r', marker='.', label='Predicted Data')
 plt.plot(actualTestData, marker='.', label = 'Actual Data')
 plt.legend()
 plt.show()
+
+#Make a prediction by using the saved model
+loadModel = load_model(savedModel)
+
+predictedBTCPrice = loadModel.predict(trainX)
+predictedBTCPrice = train_Scaler.inverse_transform(predictedBTCPrice.reshape(-1, 1))
+actualBTCPrice = train_Scaler.inverse_transform(trainY.reshape(-1, 1))
+
+plt.figure(figsize=(16,7))
+plt.title("Predicted vs Actual Train data")
+plt.plot(predictedBTCPrice, 'r', marker='.', label='Predicted Train Data')
+plt.plot(actualBTCPrice, marker='.', label = 'Actual Train Data')
+plt.legend()
+plt.show()
+
+
+#Evaluate model performance by using RMSE
+test_rmse = math.sqrt(mean_squared_error(actualTestData, predictedData))
+print('Test RMSE: %.3f' % test_rmse)
+
+train_rmse = math.sqrt(mean_squared_error(actualBTCPrice, predictedBTCPrice))
+
+print('Test RMSE: %.3f' % train_rmse)
